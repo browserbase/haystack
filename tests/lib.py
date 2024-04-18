7f3174db8ea6ec8986c78462f2d409cd3c2604c9
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2024-04-18 18:05:18 +0000

    added readme examples

--- tests/lib.py
+++ tests/lib.py
@@ -4,16 +4,19 @@ from haystack import Pipeline
 from haystack.components.generators import OpenAIGenerator
 from haystack.components.builders import PromptBuilder
 
+
 class BrowserbaseFetcherTestCase(unittest.TestCase):
     def setUp(self):
         self.browserbase_fetcher = BrowserbaseFetcher()
 
     def test_mastodon_fetcher(self):
-        result = self.browserbase_fetcher.run(urls=['https://example.com'])
-        self.assertIn("Example Domain", result['documents'][0].content)
+        result = self.browserbase_fetcher.run(urls=["https://example.com"])
+        self.assertIn("Example Domain", result["documents"][0].content)
 
     def test_mastodon_fetcher_in_pipeline(self):
-        prompt_template = "Tell me the titles of the given pages. Pages: {{ documents }}"
+        prompt_template = (
+            "Tell me the titles of the given pages. Pages: {{ documents }}"
+        )
         prompt_builder = PromptBuilder(template=prompt_template)
         llm = OpenAIGenerator()
 
@@ -24,5 +27,5 @@ class BrowserbaseFetcherTestCase(unittest.TestCase):
 
         pipe.connect("fetcher.documents", "prompt_builder.documents")
         pipe.connect("prompt_builder.prompt", "llm.prompt")
-        result = pipe.run(data={"fetcher": {"urls": ['https://example.com']}})
-        self.assertIn("1. Example Domain", result['llm']['replies'])
+        result = pipe.run(data={"fetcher": {"urls": ["https://example.com"]}})
+        self.assertIn("1. Example Domain", result["llm"]["replies"])
