dbba40b9b6a52c9bd5544d6fa56f194a1057bf3e
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-04-19 13:22:49 +0200

    updated haystack integration

--- browserbase_haystack/__init__.py
+++ browserbase_haystack/__init__.py
@@ -1,17 +1,16 @@
-import os
 from haystack import component
 from haystack.dataclasses import Document
 from browserbase import Browserbase
-from typing import List
+from typing import Optional, List, Sequence
 
 
 @component
 class BrowserbaseFetcher:
-    def __init__(self, api_key: str = os.environ["BROWSERBASE_KEY"]) -> None:
+    def __init__(self, api_key: Optional[str] = None) -> None:
         self.browserbase = Browserbase(api_key=api_key)
 
     @component.output_types(documents=List[Document])
-    def run(self, urls: List[str], text_content: bool = False):
+    def run(self, urls: Sequence[str], text_content: bool = False):
         pages = self.browserbase.load_urls(urls, text_content)
 
         documents = []
